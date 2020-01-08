30501f5e614b78b8f9703a935935bfb494c3bce5
Ken Kubo <kenkubo1990@gmail.com>
2020-01-08 11:00:11 +0900

    Hide firebase certification

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -2,6 +2,20 @@ import React from 'react';
 import { StyleSheet, View, Text, TextInput, TouchableHighlight } from 'react-native';
 
 class LoginScreen extends React.Component {
+    state = {
+        email: '',
+        password: '',
+    }
+
+    // eslint-disable-next-line
+    handleSubmit() {
+
+        // this.props.navigation.navigate('Home');
+
+        // Log in
+
+    }
+
     render() {
         return (
             <View style={styles.container}>
@@ -9,9 +23,24 @@ class LoginScreen extends React.Component {
                     ログイン
                 </Text>
 
-                <TextInput style={styles.input} value="Email Adress" />
-                <TextInput style={styles.input} value="Password" />
-                <TouchableHighlight style={styles.button} onPress={() => {}}>
+                <TextInput
+                    style={styles.input}
+                    value={this.state.email}
+                    onChangeText={(text) => { this.setState({ email: text }); }}
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    placeholder="Email Address"
+                />
+                <TextInput
+                    style={styles.input}
+                    value={this.state.password}
+                    onChangeText={(text) => { this.setState({ password: text }); }}
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    placeholder="Password"
+                    secureTextEntry
+                />
+                <TouchableHighlight style={styles.button} onPress={this.handleSubmit.bind(this)}>
                     <Text style={styles.buttonTitle}>ログインする</Text>
                 </TouchableHighlight>
             </View>
